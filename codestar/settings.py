bf2803900f60f2d369171bdf1c44fdd1f0bea958
llama2409 <nathancornwall2005@icloud.com>
2025-11-22 16:44:58 +0000

    Add updated_on field to the Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'epg4J95Rqf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
